5356639845fc59fb61e9bdc18ed3b2f2fce4f9b1
ErickShemdoe <erickshemdoe11@gmail.com>
2021-01-25 07:41:43 -0800

    In this commit I have added some codes in login and home activity. Also  I have modified layout files for the activities above.Please review.

--- app/src/main/java/com/group2/reciperepositoryapp/HomeActivity.java
+++ app/src/main/java/com/group2/reciperepositoryapp/HomeActivity.java
@@ -2,7 +2,9 @@ package com.group2.reciperepositoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Intent intent = getIntent();
+        String Name = intent.getStringExtra(LoginActivity.EXTRA_TEXT);
+        TextView Welcome_Message = (TextView) findViewById(R.id.Welcome_Message);
+
+
+       Welcome_Message.setText("Welcome "+Name);
     }
-}
+
+
+
+
+
+
+
+
+
+    }
+
--- app/src/main/java/com/group2/reciperepositoryapp/LoginActivity.java
+++ app/src/main/java/com/group2/reciperepositoryapp/LoginActivity.java
@@ -2,13 +2,48 @@ package com.group2.reciperepositoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String EXTRA_TEXT = "com.example.rentoz1.example.EXTRA_TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button Login_Button = (Button) findViewById(R.id.Login_Button);
+        Login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomepage();
+            }
+        });
+
+
+    }
+
+
+    public void openHomepage ( ) {
+        EditText editTextName = (EditText) findViewById(R.id.editTextName);
+        String name = editTextName.getText().toString();
+        EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        String password = editTextPassword.getText().toString();// Password stored as plaintext, we can encrypt them
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.putExtra(EXTRA_TEXT, name);
+
+        startActivity(intent);
+
+
     }
 }
+
+
+
+
+
+
+
