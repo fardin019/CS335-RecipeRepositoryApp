266ec3ad4a13d4fcf56a1a8b36676b826edd4b5e
ErickShemdoe <erickshemdoe11@gmail.com>
2021-02-03 15:10:48 -0800

    Some functionalities of navigation drawer added

--- app/src/main/java/com/group2/reciperepositoryapp/HomeActivity.java
+++ app/src/main/java/com/group2/reciperepositoryapp/HomeActivity.java
@@ -1,23 +1,48 @@
 package com.group2.reciperepositoryapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+
+
+
         Intent intent = getIntent();
         String Name = intent.getStringExtra(LoginActivity.EXTRA_TEXT);
         TextView Welcome_Message = (TextView) findViewById(R.id.Welcome_Message);
 
 
        Welcome_Message.setText("Welcome "+Name);
+
+
+
+       Toolbar toolbar = findViewById(R.id.toolbar);
+     // setSupportActionBar(toolbar);
+       drawer = findViewById(R.id.drawer_layout);
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this , drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+       // drawer.addDrawerListener(toggle);
+       // toggle.syncState();
+
+    }
+
+
+    public void onBackPressed(){
+
     }
 
 
--- app/src/main/java/com/group2/reciperepositoryapp/LauncherActivity.java
+++ app/src/main/java/com/group2/reciperepositoryapp/LauncherActivity.java
@@ -2,13 +2,31 @@ package com.group2.reciperepositoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LauncherActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(LauncherActivity.this , HomeActivity.class);
+                startActivity(intent);
+
+
+            }
+        },2000);
     }
 }
