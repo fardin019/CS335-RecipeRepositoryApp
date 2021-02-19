05d68b5496ac44bad20d214c903dcf5d1687d414
Cat <51199090+catherineperez46@users.noreply.github.com>
2021-02-19 10:21:58 -0800

    Update LauncherActivity.java

--- app/src/main/java/com/group2/reciperepositoryapp/LauncherActivity.java
+++ app/src/main/java/com/group2/reciperepositoryapp/LauncherActivity.java
@@ -6,37 +6,38 @@ import android.os.Bundle;
 
 public class LauncherActivity extends AppCompatActivity {
 
+   protected int SECONDS = 2;
+    Handler handler = new Handler();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_launcher);
-            Button startbtn = (Button) this.<View>findViewById(R.id.start_buttonid);
-           startbtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openLoginActivity();
 
 
-                }
-            });
-
-
-
-        }
-
-
-
-    public void openLoginActivity(){
-
-        Intent intent = new Intent(this, LoginActivity.class);
-
-
-        startActivity(intent);
-
+        // added codes
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable, 1000);
     }
+
+
+    //
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            long currentMilliseconds = System.currentTimeMillis();
+            SECONDS--;
+            if (SECONDS > 0) {
+                handler.postAtTime(this, currentMilliseconds);
+                handler.postDelayed(runnable, 1000);
+            } else {
+                Intent it = new Intent(LauncherActivity.this, LoginActivity.class);
+                startActivity(it);
+                handler.removeCallbacks(runnable);
+                finish();
+            }
+        }
+    };
+
 }
     
